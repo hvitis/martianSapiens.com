d2676d6b32fd37f95060ce7812bad276757fb66e
Adam Piskorek <adam.piskorek.pl@gmail.com>
2021-03-14 20:36:21 +0100

    Page Title

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 module.exports = {
-  title: 'Martian Sapiens', // Required
-  author: 'Hvitis', // Required
+  title: 'game about Colonizing Mars', // Required
+  author: 'Martian Sapiens', // Required
   description: 'Lets Colonize Mars Together',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://hvitis.dev',
-    github: 'https://github.com/hvitis',
     twitter: 'https://twitter.com/hvitis_',
-    linkedin: 'https://ca.linkedin.com/in/adampi',
   },
   pathPrefix: '/',
   siteUrl: 'https://martiansapiens.com',
