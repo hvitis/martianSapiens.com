390ca3d5fe87a66b39af4d3cae980e280ef832a0
Adam Piskorek <adam.piskorek.pl@gmail.com>
2021-03-14 20:30:04 +0100

    New repo

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Martian Sapiens', // Required
+  author: 'Hvitis', // Required
+  description: 'Lets Colonize Mars Together',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://hvitis.dev',
+    github: 'https://github.com/hvitis',
+    twitter: 'https://twitter.com/hvitis_',
+    linkedin: 'https://ca.linkedin.com/in/adampi',
+  },
+  pathPrefix: '/',
+  siteUrl: 'https://martiansapiens.com',
+};
